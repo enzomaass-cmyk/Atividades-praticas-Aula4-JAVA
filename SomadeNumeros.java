27c9d925bfaf4364c9407edba3c66b6bb42344d9
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-12-19 13:14:56 -0300

    exercicio 4 concluido

---
+++ SomadeNumeros.java
@@ -0,0 +1,14 @@
+public class SomadeNumeros {
+    public static void main(String[] args) {
+
+        int soma = 0;
+
+        // Somar números de 1 a 100
+        for (int i = 1; i <= 100; i++) {
+            soma += i;
+        }
+
+        System.out.println("Soma de 1 a 100: " + soma);
+
+    }
+}
