b295209eabd18c1e30fb1c2c1ad6be24f897a7a2
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-12-19 13:31:50 -0300

    exercicio 9 concluido

---
+++ ForEach.java
@@ -0,0 +1,12 @@
+public class ForEach {
+    public static void main(String[] args) {
+
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        // Usando for-each para iterar no array
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+
+    }
+}
