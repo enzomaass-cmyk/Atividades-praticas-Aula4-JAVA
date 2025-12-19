0320b3d78f899ac85980b5269c18f99eb35c4fd4
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-12-19 13:23:06 -0300

    exercicio 6 concluido

---
+++ DoWhile.java
@@ -0,0 +1,14 @@
+public class DoWhile {
+    public static void main(String[] args) {
+
+        int numero = 0;
+
+        // Usando do-while
+        do {
+            System.out.println("Número: " + numero);
+            numero++;
+        } while (numero < 5);
+
+    }
+}
+
