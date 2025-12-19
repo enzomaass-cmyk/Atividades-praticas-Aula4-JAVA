e7a520e417a091be8ee1013b1dd9bd349ce9de42
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-12-19 13:08:15 -0300

    exercício 2 concluido

---
+++ ForComDecremento.java
@@ -0,0 +1,10 @@
+public class ForComDecremento {
+    public static void main(String[] args) {
+
+        // Contar de 10 para 1
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+    }
+}
