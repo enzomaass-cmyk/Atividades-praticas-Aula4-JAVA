02d7f5f83438742fcbd45e93a3f0c5331737b1e5
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-12-19 13:17:36 -0300

    exercicio 5 concluido

---
+++ WhileSimples.java
@@ -0,0 +1,13 @@
+public class WhileSimples {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        // Contar de 1 a 5 usando while
+        while (i <= 5) {
+            System.out.println("i = " + i);
+            i++;
+        }
+
+    }
+}
