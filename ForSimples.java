d95e0478574d9d6bb78946780c34f1809203857e
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-12-19 13:03:08 -0300

    exercicio 1 concluido

---
+++ ForSimples.java
@@ -0,0 +1,10 @@
+public class ForSimples {
+    public static void main(String[] args) {
+
+        // Contar de 1 a 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
